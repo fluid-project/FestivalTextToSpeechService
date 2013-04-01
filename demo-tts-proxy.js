89a2142cc6cb933a7023cdfcc3ddbbce1fec3b2f
Yura Zenevich <yura.zenevich@gmail.com>
2013-04-01 17:07:47 -0400

    Changed to use firework

--- demo-tts-proxy.js
+++ demo-tts-proxy.js
@@ -10,8 +10,14 @@ compliance with this License.
 "use strict";
 
 var http = require("http");
+var url = require("url");
+var process = require("child_process");
 
 http.createServer(function (req, res) {
+    var query = url.parse(req.url, true);
+    var cmd = ["echo", query.q, "|", "/usr/bin/text2wave"];
+    process.spawn(cmd).pipe(res, {end: true});
+    /*
     var request = http.request({
         host: "translate.google.com",
         path: "/translate_tts" + req.url.substr(1)
@@ -19,4 +25,5 @@ http.createServer(function (req, res) {
         resp.pipe(res, {end: true});
     });
     req.pipe(request, {end: true});
+    */
 }).listen(8080);
